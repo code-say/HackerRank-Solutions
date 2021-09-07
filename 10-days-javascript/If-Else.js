b87f49a2515a0cec0705c501e864c8fb0d69f51b
sayancse <78550096+code-say@users.noreply.github.com>
2021-09-07 08:57:46 +0530

    Update If-Else.js

--- 10-days-javascript/If-Else.js
+++ 10-days-javascript/If-Else.js
@@ -26,21 +26,18 @@ function getGrade(score) {
     // Write your code here
     if(score > 25 && score <= 30){
         return("A");
-    }else if(score > 15 && score <=20){
+    }else if(score > 20 && score <=25){
         return("B");
-    }else if(score > 10 && score <=15){
+    }else if(score > 15 && score <= 20){
+        return("C");
+    }
+    else if(score > 10 && score <=15){
         return("D");
-    }else if(score > 5 && score <=5){
+    }else if(score > 5 && score <=10){
         return("E");
     }else if(score > 0 && score <= 5){
         return("F");
     }
- return grade;
+return score;
 }
 
-
-function main() {
-    const score = +(readLine());
-    
-    console.log(getGrade(score));
-}
